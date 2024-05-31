674cd2a1577a2402d6620e9002f395977650e00d
jspmern <jspmern@gmail.com>
2024-05-31 11:50:42 +0530

    assignment2 done

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-db.newemp.insertMany([
-    { name: "John Doe", salary: 50000, address: "123 Main St, Anytown, USA", course: ["HTML", "CSS", "JavaScript"] },
-    { name: "Jane Smith", salary: 60000, address: "456 Oak Ave, Sometown, USA", course: ["JavaScript", "Python", "React"] },
-    { name: "Michael Johnson", salary: 55000, address: "789 Elm St, Othertown, USA", course: ["HTML", "JavaScript", "Node.js"] },
-    { name: "Emily Brown", salary: 52000, address: "321 Maple Dr, Newville, USA", course: ["HTML", "CSS", "Java"] },
-    { name: "Daniel Lee", salary: 58000, address: "654 Pine Rd, Yourtown, USA", course: ["JavaScript", "React", "Python"] },
-    { name: "Sarah Wilson", salary: 53000, address: "987 Cedar Ln, Smalltown, USA", course: ["HTML", "CSS", "SQL"] },
-    { name: "John Doe", salary: 50000, address: "246 Birch Ave, Hometown, USA", course: ["JavaScript", "React", "Node.js"] },
-    { name: "Jessica Taylor", salary: 54000, address: "135 Spruce Blvd, Anywhere, USA", course: ["HTML", "JavaScript", "CSS"] },
-    { name: "Michael Johnson", salary: 55000, address: "864 Walnut St, Otherville, USA", course: ["HTML", "CSS", "Python"] },
-    { name: "Amanda Rodriguez", salary: 59000, address: "579 Cherry Dr, Yourville, USA", course: ["JavaScript", "React", "Node.js"] }
-])
+let assign=[{name:"sports",url:"https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?t=st=1717052346~exp=1717055946~hmac=647300f09e275c9e4f7f17770cfb9f6308378747e1a045ee29aca17ef7ffaae5&w=996"},{name:"shoes"},{name:'books'},{name:"electronic"},{name:'cloths'}]
